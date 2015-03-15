646a5086fb946c65350b87168fb4e7f7304ae1af
Morulus <vladimirmorulus@gmail.com>
2015-03-15 16:13:36 +0300

    rename snipper package

--- .dvlp/gruntfile.js
+++ .dvlp/gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-snipper');
+  grunt.loadNpmTasks('grunt-snipper');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');

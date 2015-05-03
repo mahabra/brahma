2db888c744ac6ed94de2eb47f4a8241d10cbeb6e
Morulus <vladimirmorulus@gmail.com>
2015-05-04 02:33:49 +0300

    add Brahma.ref function

--- .dvlp/scripts/brahma/hacks/refPrototype.js
+++ .dvlp/scripts/brahma/hacks/refPrototype.js
@@ -1,6 +1,19 @@
+/*
+В идеале это должно быть в прототипе, но подобная модификация вызывает проблемы совместимости с библиотеками, не проверяющими hasOwnProperty
 Object.prototype.ref = function() {
 	function Ref() {};
 	Ref.prototype = this;
+	Ref.prototype.constructor = Ref;
+	return new Ref;
+};
+
+Функция создает объект, ссылающийся на другой. Соль в том, что ссылка будет происходить через прототип, а сам конструктор объекта будет типа Ref.
+	Это позволит его отличать от обычной ссылки на объект при клонировании.
+*/
+
+Brahma.ref = function(proto) {
+	function Ref() {};
+	Ref.prototype = proto||this;
 	Ref.prototype.constructor = Ref;
 	return new Ref;
 };
